3a5fcb427b737842351c266518d382678b1ffb40
Vitor <vitorlopesfranchi@gmail.com>
2020-02-11 16:03:59 -0300

    Basic functions work with notification

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,49 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.aranha = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
+
         response.setHeader('Access-Control-Allow-Origin', '*');
         response.send({ text: "Hello from Firebase!" });
     });
 });
+
+
+exports.sendnot = functions.firestore
+    .document('portaria/{userId}')
+    .onWrite(async (change, context) => {
+        const followerUid = context.params.followerUid;
+        const followedUid = context.params.followedUid;
+        // If un-follow we exit the function.
+        console.log('We have a new follower UID:', followerUid, 'for user:', followedUid);
+
+        // Get the list of device notification tokens.
+        // Get the follower profile.
+        // The snapshot to the user's tokens.
+        // Check if there are any device tokens.
+        // Notification details.
+        const payload = {
+            notification: {
+                title: 'You have a new follower!',
+                body: `is now following you.`,
+            }
+        };
+
+        // Listing all tokens as an array.
+        // Send notifications to all tokens.
+        const response = await admin.messaging().sendToDevice("dw9TNDOf9BA:APA91bFC6WLz2fl5kg-Vf_zDmhs1ROzR5O5w1Q7_lKw6ieLNYEfTHcJ2lmgjNkIDM31G0m-Q1LvRmd9BfIoBCC20keDC-Swh8EJeRKZvYBMWo5DjDqhGwrlMrwetohXfIsbgXs4ZZnUo", payload);
+        // For each message check if there was an error.
+        const tokensToRemove = [];
+        response.results.forEach((result, index) => {
+            const error = result.error;
+            if (error) {
+                console.error('Failure sending notification to', tokens[index], error);
+                // Cleanup the tokens who are not registered anymore.
+                if (error.code === 'messaging/invalid-registration-token' ||
+                    error.code === 'messaging/registration-token-not-registered') {
+                    tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
+                }
+            }
+        });
+        return Promise.all(tokensToRemove);
+    });
+// dw9TNDOf9BA:APA91bFC6WLz2fl5kg-Vf_zDmhs1ROzR5O5w1Q7_lKw6ieLNYEfTHcJ2lmgjNkIDM31G0m-Q1LvRmd9BfIoBCC20keDC-Swh8EJeRKZvYBMWo5DjDqhGwrlMrwetohXfIsbgXs4ZZnUo
